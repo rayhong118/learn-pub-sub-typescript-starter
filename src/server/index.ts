6608cd0e3b7dcf55af96dc98b2bbdd0150a39dbb
rayhong <rayhong118@gmail.com>
2026-04-07 21:49:22 -0700

    CH2

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,19 @@
+import amqp from "amqplib";
+
 async function main() {
-  console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const connection = await amqp.connect(rabbitConnString);
+  console.log("connection successful");
+
+  // Handle Ctrl+C (SIGINT) and standard termination (SIGTERM)
+  const handleExit = async () => {
+    console.log("\nClosing RabbitMQ connection...");
+    await connection.close();
+    process.exit(0);
+  };
+
+  process.on("SIGINT", handleExit);
+  process.on("SIGTERM", handleExit);
 }
 
 main().catch((err) => {
